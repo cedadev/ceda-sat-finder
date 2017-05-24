e41755f1ea64cc076bc731545ba58f4e1e7a9064
rsmith013 <richard.d.smith@stfc.ac.uk>
2017-05-24 16:28:48 +0100

    Re-instated welcome modal

--- satellite-search-ui/js/ceda-eo.js
+++ satellite-search-ui/js/ceda-eo.js
@@ -1093,7 +1093,7 @@ window.onload = function () {
 
 
     // Open welcome modal
-    // $('#welcome_modal').modal('show')
+    $('#welcome_modal').modal('show')
 
     //------------------------------- Buttons -------------------------------
     $('#location_search').click(
